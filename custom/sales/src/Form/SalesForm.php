e2f48d141196dcad2d71a46a0ceabcb79b92bcd4
Shagun Shetty <shagunshetty@gmail.com>
2021-10-11 18:29:08 +0530

    Adding changes

--- custom/sales/src/Form/SalesForm.php
+++ custom/sales/src/Form/SalesForm.php
@@ -11,6 +11,8 @@ use Drupal\file\Entity\File;
 use Drupal\common_utilities\Utilities\commonUtil;
 
 class SalesForm extends FormBase {
+  var $path_file_tmp = 'public://attachment/tmp/boq/';
+  var $path_file_final = 'public://attachment/final/boq/';
 
   var $form_field = array(
     'date',
@@ -20,6 +22,7 @@ class SalesForm extends FormBase {
     'project_class',
     'project_type',
     'source_of_lead',
+    'source_of_lead_comment',
     'project',
     'sales_person',
     'currency',
@@ -46,6 +49,8 @@ class SalesForm extends FormBase {
     $current_roles = $current_user->getRoles();
     $form_field = $this->form_field;
     
+    $arr_engineer_list = commonUtil::getUsersByRole('sales_engineer');
+
     if ($node_id > 0) {
       // Edit Mode
       $node = Node::load($node_id);
@@ -70,11 +75,12 @@ class SalesForm extends FormBase {
           $form_data['client_email_' . $i] = $arr_client_email[($i-1)]['value'];
         }
 
-        //echo "<pre>";print_r($form_data);exit;
+        $form_data['boq_document'] = $node->get('field_boq_document')->getString();
+        //  echo "<pre>";print_r($form_data);exit;
         // get created timestamp
         $created_timestamp = 'Created on ' . date("d F Y H:i", $node->getCreatedTime());
       } else {
-          commonUtil::my_goto_error();
+          my_goto_error();
           return;
       }
     } else {
@@ -85,6 +91,7 @@ class SalesForm extends FormBase {
       foreach ($form_field as $field_name) {
         $form_data[$field_name] = '';
       }
+      $form_data['boq_document'] = '';
 
       for($i=1; $i<=4; $i++) {
         $form_data['client_name_' . $i] = '';
@@ -228,7 +235,7 @@ class SalesForm extends FormBase {
       '#options' => $arr_location, 
       '#title' => $this->t('Location'),
       '#default_value' => $form_data['location'],
-    ];  
+    ];
 
     $arr_deal_hotness = array(
     'hot' => 'Hot',
@@ -257,7 +264,9 @@ class SalesForm extends FormBase {
     'a_class' => 'A Class (Services / Software / AMC)',
     'b_class' => 'B Class( Design Build)',
     'c_class' =>'C Class (Build Bid with alternates allowed)',
-    'd_class' => 'D Class (Strict RFP)'
+    'd_class' => 'D Class (Strict RFP)',
+    'upgrade' => 'Upgrade',
+    'upsales' => 'Upsales',
     );
 
     $form['sales_info']['project_class'] = [
@@ -278,6 +287,10 @@ class SalesForm extends FormBase {
     'residential' => 'Residential',
     'professional_services' => 'Professional Services',
     'amc' => 'AMC',
+    'government' => 'Government',
+    'townhall_event_spaces' => 'Townhall / Event Spaces',
+    'boardrooms_and_conference_rooms' => 'Boardrooms and Conference Rooms',
+    'experience_center' => 'Experience Center',
     'others' => 'Others',
     );
 
@@ -288,14 +301,29 @@ class SalesForm extends FormBase {
       '#default_value' => $form_data['project_type'],
     ];
     
+    $arr_source_of_lead = array(
+      'lead_from_allwave_team'  => 'Lead from allwave team',
+      'website'  => 'Website',
+      'reference'  => 'Reference',
+      'cold_calling_self_generated'  => 'Cold Calling / Self Generated',
+      'existing_client'  => 'Existing Client',
+      'other'  => 'Other',
+    );
+
     $form['sales_info']['source_of_lead'] = [
-      '#type' => 'textfield',
+      '#type' => 'select',
+      '#options' => $arr_source_of_lead, 
       '#title' => $this->t('Source of Lead'),
-      '#required' => TRUE,        
+      '#default_value' => $form_data['source_of_lead'],
+    ];
+    
+    $form['sales_info']['source_of_lead_comment'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Source of Lead Comment	'),
       '#attributes' => array(
           'autocomplete' => 'nope',
       ),
-      '#default_value' => $form_data['source_of_lead'],
+      '#default_value' => $form_data['source_of_lead_comment'],
     ];
 
     $form['sales_info']['client_info'] = array(
@@ -412,13 +440,11 @@ class SalesForm extends FormBase {
     ];
 
     $form['sales_info']['engineer'] = [
-      '#type' => 'textfield',
+      '#type' => 'select',
+      '#options' => $arr_engineer_list, 
       '#title' => $this->t('Design & Estimation Engineer'),
-      '#attributes' => array(
-          'autocomplete' => 'nope',
-      ),
       '#default_value' => $form_data['engineer'],
-    ];
+    ];  
 
     $form['sales_info']['boq_person'] = [
       '#type' => 'textarea',
@@ -429,6 +455,53 @@ class SalesForm extends FormBase {
       '#default_value' => $form_data['boq_person'],
     ];
     
+    $form['sales_info']['boq_document'] = array(
+      '#type' => 'managed_file',
+      '#title' => $this->t('BOQ Document'),
+      '#multiple' => true,
+      '#progress_message' => $this->t('Please wait...'),
+      '#size' => 20,
+      '#description' => t('Allowed file type : pdf xls xlsx doc docx'),
+      '#upload_validators' => array('file_validate_extensions' => 'pdf xls xlsx doc docx'),
+      '#upload_location' => $this->path_file_tmp,
+    );
+
+    // List files
+    $arr_delete_file_list = array();
+    if ($form_data['boq_document'] != '') {
+      $arr_file_list = unserialize($form_data['boq_document']);
+      if (is_array($arr_file_list) && count($arr_file_list) > 0) {
+        foreach ($arr_file_list as $val_file_name) {
+          $file_url = file_create_url($this->path_file_final);
+          //Check if file exist
+          $final_realpath = \Drupal::service('file_system')->realpath($this->path_file_final);
+
+          $final_realpath = $final_realpath . "/".$node_id."/".$key_file_upload.$val_file_name;
+
+          if (file_exists($final_realpath)) {
+            $file_url = $file_url."/".$node_id."/".$key_file_upload."/".$val_file_name;
+            $val_file_name_with_time = $this->_getFileNameWithTime($val_file_name);
+            $arr_delete_file_list[$val_file_name] = Link::fromTextAndUrl($val_file_name_with_time, Url::fromUri($file_url,
+              array('attributes' => array('target' => '_blank'))))->toString();
+          }
+        }
+        $form['sales_info']['chk_boq_document'] = array(
+          '#title' => t('File List'),
+          '#type' => 'checkboxes',
+          '#options' => $arr_delete_file_list,
+        );
+
+        $form['sales_info']['delete_boq_document'] = array(
+          '#type' => 'submit',
+          '#value' => t('Delete file'),
+          '#name' => 'btn_delete_boq_document',
+          '#attributes' => array('onclick' => 'if(!confirm("Do you really want to delete the selected file?")){return false;}'),
+          '#submit' => array('::submitDeleteFile'),
+        );
+      }
+    }
+
+
     $arr_deal_stage = array(
     'suspect' => 'Suspect',
     'prospect' => 'Prospect',
@@ -498,6 +571,7 @@ class SalesForm extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $file_field = 'boq_document';
     if ($form_state->getValue('node_id') > 0) {
       // Update Record
       $node_id = $form_state->getValue('node_id');
@@ -514,6 +588,20 @@ class SalesForm extends FormBase {
         $node->set($field_name, $field_value);
       }
 
+      // Save files
+      $arr_file_list = $this->_uploadFile($file_field, $node_id, $form_state);
+      if (is_array($arr_file_list) && count($arr_file_list) > 0) {
+        // Load the existing files
+        $str_stored_file_list = trim($node->get('field_'.$file_field)->getString());
+        if ($str_stored_file_list != '') {
+          $arr_stored_file_list = unserialize($str_stored_file_list);
+          // Append to existing list.
+          $arr_file_list = array_merge($arr_file_list, $arr_stored_file_list);
+        }
+
+        $node->set('field_'.$file_field, serialize($arr_file_list));
+      }
+
       $node->save();
       \Drupal::messenger()->addStatus("Record has been updated!\n");
 
@@ -527,6 +615,13 @@ class SalesForm extends FormBase {
   
       $node->save();
       $node_id = preg_replace("/[^0-9]/", "", $node->id());
+
+      // Save files
+      $arr_file_list = $this->_uploadFile($file_field, $node_id, $form_state);
+      if (is_array($arr_file_list) && count($arr_file_list) > 0) {
+        $node->set('field_'.$file_field, serialize($arr_file_list));
+      }
+
       \Drupal::messenger()->addStatus("Record has been created!\n");
           commonUtil::my_goto(Url::fromRoute('sales.manage',
                               array('node_id' => $node_id))->toString());
@@ -557,5 +652,124 @@ class SalesForm extends FormBase {
 
     $form_data['type'] = 'sales';
     return $form_data;
+  }
+
+  private function _uploadFile($file_field, $node_id, FormStateInterface $form_state) {
+    $arr_result = array();
+    $arr_fid = $form_state->getValue([$file_field]);
+    echo "IN".$file_field;
+    print_r($arr_fid);
+
+    if (is_array($arr_fid) && count($arr_fid)) {
+      foreach($arr_fid as $fid) {
+        $oNewFile = File::load($fid);
+        $oNewFile->save();
+        $og_file_uri = $oNewFile->getFileUri();
+
+        # Get file name & extension.
+        $path_parts = pathinfo($og_file_uri);
+        $file_name = $path_parts['filename'];
+        $file_extension = $path_parts['extension'];
+
+        # Get tmp path
+        $tmp_full_path = \Drupal::service('file_system')->realpath($og_file_uri);
+
+        # Set final  path
+        $final_realpath = \Drupal::service('file_system')->realpath($this->path_file_final);
+        $final_full_path = $final_realpath ."/".$node_id;
+
+        # Create directory if not exist.
+        if (!is_dir($final_full_path)) {
+          var_dump(mkdir($final_full_path, 0777, true));
+        }
+
+        $final_file_name = commonUtil::clean_string($file_name)."_".time().".".$file_extension;
+
+        $final_full_path = $final_full_path.'/'.$final_file_name;
+
+        if (copy($tmp_full_path, $final_full_path)){
+          $arr_result[] = $final_file_name;
+        }
+      }
+    }
+    return $arr_result;
+  }
+
+  private function _getFileNameWithTime($file_name) {
+    $datetime = '';
+    if (trim($file_name) != '') {
+      $arr_file_name = explode("_", $file_name);
+      $arr_file_name = explode(".", $arr_file_name[(count($arr_file_name)-1)]);
+      $datetime = date('m/d/Y H:i:s', $arr_file_name[0]);
+    }
+    return $file_name . " - (".$datetime.")";
+  }
+
+  public function submitDeleteFile(array &$form, FormStateInterface $form_state) {
+    $form_data_file = $this->form_data_file;
+
+    // Get the clicked form element to set the field name.
+    $clickedElement = $form_state->getTriggeringElement()['#name'];
+    $field_name = str_replace('btn_delete_', '', $clickedElement);
+
+    if ($form_state->getValue('node_id') > 0) {
+      $node_id = $form_state->getValue('node_id');
+      $node = Node::load($node_id);
+      $node_file_data = array();
+      $arr_chk_list = array();
+      $str_success_message = '';
+      $flag_message_error = false;
+      if ($node != NULL) {
+        $node_file_data[$field_name] = unserialize($node->get('field_'.$field_name)->getString());
+        $flag_file_deleted = false;
+        if(is_array($node_file_data[$field_name]) && count($node_file_data[$field_name])) {
+          $arr_chk_list = $form_state->getValue('chk_'.$field_name);
+          foreach($node_file_data[$field_name] as $val_file_name) {
+            if ($arr_chk_list[$val_file_name]) {
+
+              if (($key = array_search($val_file_name, $node_file_data[$field_name])) !== false) {
+
+                  $final_realpath = \Drupal::service('file_system')->realpath($this->path_file_final);
+                  $final_full_path = $final_realpath ."/".$node_id;
+
+                  if ($this->_deleteFile($final_full_path."/".$val_file_name)) {
+                    unset($node_file_data[$field_name][$key]);
+                    $flag_file_deleted = true;
+                    $str_success_message.= "Successfully deleted ".$val_file_name ."  ";
+                  } else {
+                    $str_success_message.= "Could not deleted ".$val_file_name ."  ";
+                    $flag_message_error = true;
+                  }
+              }                
+            }
+          }
+          // Update only when a file is deleted.
+          if ($flag_file_deleted == true) {
+            $node->set('field_'.$field_name, serialize($node_file_data[$field_name]));
+          }
+        }
+
+        $node->save();
+        if ($flag_message_error == true) {
+          \Drupal::messenger()->addWarning($str_success_message);
+        } else {
+          \Drupal::messenger()->addStatus($str_success_message);
+        }
+      } else {
+        \Drupal::messenger()->addWarning("Sorry, error processing the request!\n");
+      }
+    }    
+  }
+
+  private function _deleteFile($file_pointer) {
+    if(file_exists($file_pointer)) {
+      if (!unlink($file_pointer)) { 
+        return false;
+      } else { 
+        return true;
+      }
+    } else {
+      return false;
+    }
   }
 }
